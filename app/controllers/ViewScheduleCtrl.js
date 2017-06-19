0be7a32dd629a8d19e5756a997ac91dee0426beb
Jackie Knight <jnk08@outlook.com>
2017-06-19 13:25:00 -0500

    getting stylists in table

--- app/controllers/ViewScheduleCtrl.js
+++ app/controllers/ViewScheduleCtrl.js
@@ -9,9 +9,22 @@ app.controller("ViewScheduleCtrl", function($scope, DataFactory, $location, $rou
 		.then((appts) => {
 			$scope.appts = appts;
 			console.log("All Appointments", $scope.appts);
+			//console.log("Stylist Data", $scope.appts[0].stylistID);
+			//console.log("Time Data", $scope.appts[0].startTime);
+			//$scope.stylist = $scope.appts[0].stylistID;
+		});
+	};
+
+	$scope.getStylists = function(){
+		DataFactory.getStylists()
+		.then( (data) => {
+			console.log("Stylist Data", data);
+			$scope.stylists = data;
 		});
 	};
 
 	$scope.getAppointments();
+	$scope.getStylists();
 
 });
+
